00564ede5a1fd0962e0a4d8137dedece4b37dba0
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2026-02-22 12:37:22 +0100

    first commit

---
+++ Acronimi.js
@@ -0,0 +1,123 @@
+// =======================
+// ACRONIMI – TEST
+// =======================
+
+//todo Questa verifica serve per testare la conoscenza degli acronimi più comuni in ambito web development. 
+//todo Prova a scrivere il significato di ciascun acronimo elencato di seguito. 
+//todo Se trovi acronimi doppi, non riscriverli
+
+// DOM
+// SEO
+// URL
+// CSS
+// SCSS
+// PX
+// REM
+// EM
+// VH
+// VW
+// HTTP
+// HTTPS
+// API
+// JSON
+// AJAX
+// REST
+// ES6
+// NPM
+// DOM
+// JSON
+// React
+// JSX
+// SPA
+// CSR
+// SSR
+// HOC
+// DOM
+// Node
+// NPM
+// REPL
+// ES
+// Express
+// REST
+// CRUD
+// JWT
+// SQL
+// DB
+// RDBMS
+// CRUD
+// PK
+// FK
+// DDL
+// DML
+// DCL
+
+//! Risposte:
+
+// DOM: Document Object Model
+// SEO: Search Engine Optimition
+// URL: Uniform Research Locator 
+// CSS: Cascading Sheets Style
+// SCSS: Sassy Cascading Sheets Style
+// PX: Pixel
+// REM: Root Em
+// EM: Font size relativo al genitore
+// VH: Viewport Height
+// VW: Viewport Width
+// HTTP: Hyper Text Transfer Protocol 
+// HTTPS: Hyper Text Transfer Protocol Secure
+// API: Application Programming Interface
+// JSON: Javascript Object Notation
+// AJAX: Asyncronus Javascript and Xml 
+// REST: Reppresentational State Transfer
+// ES6: ECMAScript 6
+// NPM: Node Package Manager
+// JSX: Javascript Xml
+// SPA: Single Page Application
+// CSR: Client-Side Rendering
+// SSR: Server-Side Rendering
+// HOC: High Order Component
+// REPL: Reppresentational Event Loop
+// CRUD: Create Read Update Delete
+// JWT: Json Web Token
+// SQL: Structured Query Language
+// DB: Data Base
+// RDBMS: Relation Data Base Manager System
+// PK: Primary Key
+// FK: Foreign Key
+// DDL: Data Definition Language
+// DML: Data Manipultation Language
+// DCL: Data Control Language
+
+//! Valutazione: 
+//* Totale acronimi: 34
+
+//* Totalmente corrette: 25
+
+//! Totalmente sbagliate (concetto errato): 4
+
+// URL
+// SCSS
+// HOC
+// REPL
+
+//? Solo errori di ortografia / wording: 5
+
+// SEO
+// CSS
+// AJAX
+// REST
+// RDBMS
+
+//! Risultato: livello molto buono, errori concettuali pochissimi.
+
+//Correzione errori concettuali:
+
+// SEO: Search Engine Optimization ❌ (non Optimition)
+// URL: Uniform Resource Locator ❌ (non Research)
+// CSS: Cascading Style Sheets ❌ (ordine)
+// SCSS: Sassy CSS ❌ (non Cascading Sheets Style)
+// AJAX: Asynchronous JavaScript And XML ❌ (ortografia)
+// REST: Representational State Transfer ❌ (una P sola)
+// HOC: Higher Order Component ❌ (non High)
+// REPL: Read Eval Print Loop ❌ (non Event Loop)
+// RDBMS: Relational Database Management System ❌
